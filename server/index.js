39f7093898015c2ecf67ec53d374989063fdbf28
Mathilde <mat.Grimal@gmail.com>
2021-12-15 21:52:24 +0100

    add ref to react build in express server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.urlencoded({ extended: true }));
 
 // eslint-disable-next-line no-unused-vars
@@ -15,5 +16,8 @@ app.get('/test', (req, res, next) => res.send('test'));
 app.get('/test2/:id', (req, res, next) => res.send(`test2${req.params.id}`));
 app.post("/questions", (req, res) => res.send(req.body));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 const port = 8000;
 app.listen(process.env.PORT || port, () => { console.log(`app is running on port ${port}`); });
