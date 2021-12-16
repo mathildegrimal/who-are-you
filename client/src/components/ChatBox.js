b6d5f30a593a5dc3aa5def623ed3184831674e50
Mathilde <mat.Grimal@gmail.com>
2021-12-16 11:30:15 +0100

    moved socket in chat component on client

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import "./ChatBox.css"
+import "./ChatBox.css";
+import { io } from "socket.io-client";
+
 function ChatBox() {
+    const socket = io();
+    socket.on("connection", (message) => {
+      console.log("received message from server : ", message);
+    });
+    socket.emit("confirm", null);
+
+    const handleKeyUp = (e) => {
+        console.log(e.code);
+        if (e.code === 'Enter') {
+            socket.emit('message', e.target.value);
+        }
+        console.log(e.target.value)
+    }
     return (
-        <div className="chatbox">
-            
-        </div>
-    )
+      <div className="chatbox">
+        <input
+          type="text"
+          onKeyUp={handleKeyUp}
+        ></input>
+      </div>
+    );
 }
 
 export default ChatBox
